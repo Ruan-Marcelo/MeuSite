f276323b3965dc90e60d73f228e30085090338f2
Ruan-Marcelo <luzruan60@gmail.com>
2025-03-20 15:44:41 -0300

    inserção de PHP para HEADER e FOOTER

---
+++ PHP/footer.php
@@ -0,0 +1,40 @@
+<footer>
+      <div class="interface">
+        <div class="line-footer">
+            <div class="flex">
+            <div class="logo-footer">
+              <img src="./imagens/Captura de tela 2024-08-09 131238.png" alt="Meu Logo Ruan">
+            </div>
+            <div class="btn-social">
+              <a
+                href="https://www.instagram.com/ruan.luzzz/"
+                target="_blank"
+                rel="noopener noreferrer"
+                ><button><i class="bi bi-instagram"></i></button
+              ></a>
+              <a
+                href="https://github.com/Ruan-Marcelo"
+                target="_blank"
+                rel="noopener noreferrer"
+                ><button><i class="bi bi-github"></i></button
+              ></a>
+              <a
+                href="https://www.linkedin.com/in/ruan-marcelo/"
+                target="_blank"
+                rel="noopener noreferrer"
+                ><button><i class="bi bi-linkedin"></i></button
+              ></a>
+            </div>
+        </div>
+        <!-- line-footer -->
+      </div>
+        <div class="line-footer borda">
+          <p style="color:aliceblue;"><i class="bi bi-stars"></i><a href="./TelaDeAvaliação/index.html">Avaliar meu site!</a</p>
+          <p><i class="bi bi-envelope-paper-heart-fill"></i> <a href="mailto:luzruan60@gmail.com"target="_blank">luzruan60@gmail.com</a></p>
+          <p class="direitos">&#169; Todos os direitos reservados</p>
+        </div>
+        <!-- line-footer -->
+      </div>
+      <!-- interface -->
+    </footer>
+    <!-- rodapé -->
