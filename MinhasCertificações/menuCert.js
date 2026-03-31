d00d3e22c0b6dfc8262a4b93353d76885b30bc26
Ruan-Marcelo <luzruan60@gmail.com>
2026-03-31 14:10:59 -0300

    atimização page certificações para json e js

--- MinhasCertificações/menuCert.js
+++ MinhasCertificações/menuCert.js
@@ -19,3 +19,34 @@ overley.addEventListener("click", () => {
   menu.classList.remove("abrir-menu");
   overley.style.display = "none"; // Oculta o overlay
 });
+
+//carrega as certificações do JSON e renderiza na page bb
+fetch("certifications.json")
+  .then(response => response.json())
+  .then(data => {
+    const container = document.getElementById("certifications-container");
+
+    data.forEach(cert => {
+      const card = document.createElement("article");
+      card.classList.add("card");
+
+      card.innerHTML = `
+        <img src="../imagens/${cert.imagem}" alt="${cert.instituicao}" loading="lazy">
+
+        <h3>
+          <img src="../imagens/${cert.icone}" alt="${cert.titulo}">
+          ${cert.titulo}
+        </h3>
+
+        <ul>
+          <li><strong>Instituição:</strong> ${cert.instituicao}</li>
+          <li><strong>Conclusão:</strong> ${cert.data}</li>
+        </ul>
+
+        <p>${cert.descricao}</p>
+      `;
+
+      container.appendChild(card);
+    });
+  })
+  .catch(error => console.error("Erro ao carregar certificações:", error));
